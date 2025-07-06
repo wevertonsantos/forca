3b4e4d3ae13161528167e73e72f1cb8d8e7055f0
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-06 11:30:06 -0300

    começando o jogo.

---
+++ forca.py
@@ -0,0 +1,19 @@
+"""
+O programa escolhe uma palavra secreta (pode ser fixada no código ou sorteada de uma lista).
+
+O jogador tenta adivinhar essa palavra letra por letra.
+
+Para cada letra correta, o programa revela a letra na posição correta na palavra.
+
+Para cada letra errada, o programa registra um erro.
+
+O jogador tem um número limitado de erros (exemplo: 6 chances).
+
+Se o jogador adivinhar todas as letras antes de acabar as chances, ele vence.
+
+Se acabar as chances antes de descobrir a palavra, perde.
+"""
+
+# variável com a palavra secreta
+palavra_secreta = "python"
+
