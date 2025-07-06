64abc0d60cd24ab796356c9279216c8c211de257
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-06 13:14:41 -0300

    verificando se a chave letra tem valor True, pegando o índice de cada elemento do dicionário e colocando a letra na respectiva posição

--- forca.py
+++ forca.py
@@ -16,7 +16,8 @@ Se acabar as chances antes de descobrir a palavra, perde.
 
 # variável com a palavra secreta
 palavra_secreta = "python"
-
+lista_palavra_secreta = ["_","_","_","_","_"]
+letras_python = {"p":False,"y":False,"t":False,"h":False,"o":False,"n":False}
 palavra = ""
 erros = 0
 
@@ -25,6 +26,7 @@ while True:
     if palavra != palavra_secreta:
         # entrada do usuario
         letra = input("Digite uma letra: ")
+        print(palavra)
     else:
         print(f"Você achou a palavra correta: {palavra_secreta}")
         break
@@ -32,13 +34,25 @@ while True:
     if len(letra) == 1 and letra.isalpha():
         # verificando se a letra está na palavra secreta
         if letra in palavra_secreta:
-            palavra += letra
+            letras_python[letra] = True
+            # pegando cada letra do dic
+            for letra in letras_python.keys():
+                # verificando se a chave letra tem valor True
+                if letras_python[letra] == True:
+                    # pegando o índice de cada elemento do dicionário
+                    indice_dic = list(letras_python.keys()).index(letra)
+                    # colocando a letra na respectiva posição
+                    lista_palavra_secreta[indice_dic] = letra
+            for letra in lista_palavra_secreta:
+                palavra += letra
+            print(palavra)
         else:
             # acaba o programa caso tenha 6 erros
             if erros == 5:
                 print("As tentativas acabaram. Fim de jogo!")
                 break
             else:
+                print(palavra)
                 # aumenta o erro caso erre
                 erros += 1
     else:
